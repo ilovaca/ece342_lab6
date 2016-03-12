88a995cac2e7a8feefdefc26f6838b0aabf0c50e
austin <1219468750@qq.com>
2016-03-12 18:02:07 -0500

    dfas

--- proc.v
+++ proc.v
@@ -25,125 +25,157 @@ module proc (DIN, Resetn, Clock, Run, Done, BusOutput);
 	always @(Tstep_Q or Opcode or Rx or Ry)	
 	begin
 		// . . . specify initial values
-		case (Tstep_Q)
-			2'b00: // store DIN in IR in time step 0
-				begin
-					ld_IR = 1'b1;
-					R_sel = 8'bxxxx_xxxx;
-					ld_EN = 0;
-					ld_sel = 8'bxxxx_xxxx;
-					Done = 0;
-					bus_EN = 0;
-				end
-			2'b01: //define signals in time step 1
-				case (Opocde)
-					MV: begin
-						ld_IR = 0;
-						R_sel = Ry;
-						bus_EN = 1;
-						ld_EN = 1;
-						ld_sel = Rx;
+		if (Resetn) begin
+			Clear = 1;
+			ld_IR = 0;
+			ld_EN = 0;
+			ld_Ain = 0;
+			ld_G = 0;
+			R_sel = 8'bxxxx_xxxx;
+			ld_sel = 8'bxxxx_xxxx;
+			bus_reg_sel = 0;
+			bus_Gout_sel = 0;
+			bus_DIN_sel = 0;
+			Done = 0;
+		end
+		else begin
+			case (Tstep_Q)
+				2'b00: // store DIN in IR in time step 0
+					begin
+						Clear = 0;
+						ld_IR = 1;
+						ld_EN = 0;
 						ld_Ain = 0;
-						Done = 1;
-					end
-					MVI: begin
-						ld_IR = 0;
+						ld_G = 0;
 						R_sel = 8'bxxxx_xxxx;
-						BusWires = DIN;
-						ld_EN = 1;
-						ld_sel = Rx;
-						ld_Ain = 0;
-						Done = 1;
-					end
-					ADD: begin
-						ld_IR = 0;
-						R_sel = Rx;
-						ld_EN = 0;
 						ld_sel = 8'bxxxx_xxxx;
-						ld_Ain = 1;
+						bus_reg_sel = 0;
+						bus_Gout_sel = 0;
+						bus_DIN_sel = 1;
+						Done = 0;
 					end
-					SUB: begin
-						ld_IR = 0;
-						R_sel = Rx;
-						ld_EN = 0;
-						ld_sel = 8'bxxxx_xxxx;
-						ld_Ain = 1;
-						
-					end
-					default: begin
-						ld_Ain = 1'bx;
-						ld_R = 1'bx;
-						ld_EN = 0;
+				2'b01: 
+					case (Opocde)
+						MV: begin
+							ld_IR = 0;
+							R_sel = Ry;
+							bus_reg_sel = 1;
+							ld_EN = 1;
+							ld_sel = Rx;
+							ld_Ain = 0;
+							Done = 1;
+						end
+						MVI: begin
+							ld_IR = 0;
+							R_sel = 8'bxxxx_xxxx;
+							BusWires = DIN;
+							ld_EN = 1;
+							ld_sel = Rx;
+							ld_Ain = 0;
+							Done = 1;
+						end
+						ADD: begin
+							ld_IR = 0;
+							R_sel = Rx;
+							ld_EN = 0;
+							ld_sel = 8'bxxxx_xxxx;
+							ld_Ain = 1;
+						end
+						SUB: begin
+							ld_IR = 0;
+							R_sel = Rx;
+							ld_EN = 0;
+							ld_sel = 8'bxxxx_xxxx;
+							ld_Ain = 1;
+							
+						end
+						default: begin
+							ld_Ain = 1'bx;
+							ld_R = 1'bx;
+							ld_EN = 0;
 
-						ld_sel = 8'bxxxx_xxxx;
-					end
-				endcase
-			2'b10: //define signals in time step 2
-				case (Opcode)
-					MV: begin
-						/*set zero*/
-						ld_IR = 0;
-						ld_EN = 0;
-						ld_sel = 8'bxxxx_xxxx;
-						ld_Ain = 0;
-						ld_G = 0;
-					end
-					MVI: begin
-						/*set zero*/
-						ld_IR = 0;
-						ld_EN = 0;
-						ld_sel = 8'bxxxx_xxxx;
-						
-						ld_Ain = 0;
-						ld_G = 0;
-					end 
-					ADD: begin
-						// cycle 2 we place Ry on the bus
-						R_sel = Ry;
-						ld_G = 1;
-					end
-					SUB: begin
-						
-					end
-					default: begin
-						
-					end
-				endcase
-			2'b11: //define signals in time step 3
-				case (Opcode)
-					MV: begin
-						
-					end
-					MVI: begin
-						
-					end
-					ADD: begin
-						
-					end
-					SUB: begin
-						BusWires = G_out;
-						ld_sel = Rx;
-						Done = 1;
-					end
-					default:
-				endcase
-		endcase
+							ld_sel = 8'bxxxx_xxxx;
+						end
+					endcase
+				2'b10: //define signals in time step 2
+					case (Opcode)
+						MV: begin
+							/*set zero*/
+							ld_IR = 0;
+							ld_EN = 0;
+							ld_sel = 8'bxxxx_xxxx;
+							ld_Ain = 0;
+							ld_G = 0;
+						end
+						MVI: begin
+							/*set zero*/
+							ld_IR = 0;
+							ld_EN = 0;
+							ld_sel = 8'bxxxx_xxxx;
+							
+							ld_Ain = 0;
+							ld_G = 0;
+						end 
+						ADD: begin
+							// cycle 2 we place Ry on the bus
+							R_sel = Ry;
+							ld_G = 1;
+						end
+						SUB: begin
+							
+						end
+						default: begin
+							
+						end
+					endcase
+				2'b11: //define signals in time step 3
+					case (Opcode)
+						MV: begin
+							
+						end
+						MVI: begin
+							
+						end
+						ADD: begin
+							
+						end
+						SUB: begin
+							BusWires = G_out;
+							ld_sel = Rx;
+							Done = 1;
+						end
+						default:
+					endcase
+			endcase
+		end
 	end
 
 	// assign R_sel = X_sel? Rx: Ry ;
 	/************************** BUS mux ********************************/
 	always @ (*) begin
-		case(R_sel) 
-			8'b0000_0000: if (bus_EN) BusWires = R0;
-			8'b0000_0010: if (bus_EN) BusWires = R1; 
-			8'b0000_0100: if (bus_EN) BusWires = R2;
-			8'b0000_1000: if (bus_EN) BusWires = R3;
-			8'b0001_0000: if (bus_EN) BusWires = R4;
-			8'b0010_0000: if (bus_EN) BusWires = R5;
-			8'b0100_0000: if (bus_EN) BusWires = R6;
-			8'b1000_0000: if (bus_EN) BusWires = R7;
-		endcase
+		if (bus_reg_sel) begin
+			case(R_sel) 
+				8'b0000_0000: BusWires = R0;
+				8'b0000_0010: BusWires = R1; 
+				8'b0000_0100: BusWires = R2;
+				8'b0000_1000: BusWires = R3;
+				8'b0001_0000: BusWires = R4;
+				8'b0010_0000: BusWires = R5;
+				8'b0100_0000: BusWires = R6;
+				8'b1000_0000: BusWires = R7;
+			endcase
+		end
+		else if (bus_Gout_sel) begin
+				BusWires = G_out;
+		end
+		else if (bus_DIN_sel) begin
+				BusWires = DIN;
+		end
+		else 	// should not happen
+				BusWires = 16'bxxxx_xxxx_xxxx_xxxx;
 	end	
+
+	/************************* Register Load Enable ********************/
 	always @ (*)
 		case(ld_sel)
 			8'b0000_0000: ld_R[0] = ld_EN;
